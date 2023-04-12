3b6f0d59e23c04412576508d56433efd354eb272
yvnji <yvnji25@gmail.com>
2023-04-12 23:39:27 +0900

    update CSS styling and JavaScript code

--- js/main.js
+++ js/main.js
@@ -4,15 +4,11 @@ const optionsBtn = document.querySelector(".options-button");
 
 const mouseOver = (e) => {
     let buttonClass = e.target;
-    buttonClass.style.color="#000";
-    buttonClass.style.boxShadow = "0 0 0 white";
     buttonClass.classList.add('on');
 }
 
 const mouseOut = (e) =>{
     let buttonClass = e.target;
-    buttonClass.style.color = "#fff";
-    buttonClass.style.boxShadow = "4px 4px 1px brown";
     buttonClass.classList.remove("on");
 }
 
@@ -21,4 +17,3 @@ playBtn.addEventListener('mouseout', mouseOut);
 
 optionsBtn.addEventListener('mouseover', mouseOver);
 optionsBtn.addEventListener("mouseout", mouseOut);
-
