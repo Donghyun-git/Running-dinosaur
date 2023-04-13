a205f81c06fffa3b9e620d494eb634c619975561
donghyun-git <donghyun1113@hanmail.net>
2023-04-13 22:19:51 +0900

    make fireball in canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -2,17 +2,37 @@ const canvas = document.querySelector('.canvas');
 const ctx = canvas.getContext('2d');
 
 class User {  //유저 클래스 정의
-    constructor(x, y, isJump, attack, keyStack){
+    constructor(x, y, isJump, isAttack, keyStack){
         this.x = x;  //캔버스 x좌표 값
         this.y = y;  //캔버스
         this.isJump = isJump; //스페이스바 키 다운 여부
-        this.attack = attack; // 컨트롤키 다운 여부.
+        this.isAttack = isAttack; // 컨트롤키 다운 여부.
         this.keyStack = keyStack; //스페이스바 키 몇번눌렀는지 카운트
     }
 }    //이게 캔버스 width랑 height를 정의해놓고 css로 건드려놔서 실제 좌표랑 달러. 그래서 아까 클릭이벤트로 좌표값 찍어본거야 ..ㅠㅠ
 
+class Fire extends User {
+  constructor(x, y, isJump, isAttack, keyStack, fx, isConflict) {
+    super(x,y, isAttack);
+    this.isJump = isJump;
+    this.keyStack = keyStack;
+    this.fx = fx;
+    this.isConflict = isConflict;
+  }
+  
+}
+
+class SmallBlock {  //작은 장애물 
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
 const user = new User(80, 605, false, false, 2);  // 유저 인스턴스 생성, (초기 x값, 초기 y값, 점프키 누름 여부, 컨트롤키 누름여부, 스페이스바 키 스택 초기값 == 0)
-let spaceCount = 0;
+const blocks = new SmallBlock(100, 100);
+const attackList = [];
+const blocksList = [];
 
 const userImage = new Image();
 userImage.src = "../img/user.png";
@@ -21,9 +41,14 @@ userImage.onload = function () {
   setUser();
 };
 
+const getRadian = (d) => {
+  return d * 180 / Math.PI;
+}
+
 const setUser = () => {
   ctx.clearRect(0, 0, 3000, 3000) // 매 프레임 재귀 돌때마다 캔버스 지움
-  ctx.drawImage(userImage, user.x, user.y, 150, 200)
+  ctx.drawImage(userImage, user.x, user.y, 150, 200);
+
 //   ctx.fillStyle = "red";
 //   ctx.fillRect(user.x, user.y, 100, 100); // 용용이 히트박스  
     if (user.isJump === true) {
@@ -34,23 +59,58 @@ const setUser = () => {
         user.isJump = false;
       }
     }
-  
+
   if(user.y < 605 && user.isJump === false){ // 용용이가 땅바닥 위에 있고 isJump 가 false면은 용용이 내려감. 근데 이제 700 이하로 계속 떨어지기 때문에 첫번째 조건에서 걸려서 땅바닥에 착지하면 더이상 안내려가는거양 
     user.y += 8; //여기서 +8을 계속 해주고 있자나
   }
-
 }
+
+
+
+ 
+document.addEventListener("keydown", (e) => {
+  if (e.ctrlKey) {
+    const fire = new Fire(80, user.y, false, false, 0, 210, false);
+    attackList.push(fire);
+    console.log(attackList);
+  }
+});
+
+const fireBall = new Image();
+fireBall.src = '../img/fireball.png';
+fireBall.onload = function () {
+  // 이미지가 로드된 후 실행되는 함수
+  setAttack();
+};
+
+
+const setAttack = () => {
+  ctx.fillStyle = "red";
+  attackList.forEach(item => {
+    ctx.beginPath();
+    ctx.drawImage(fireBall, item.fx, item.y + 56, 50, 50);
+    // ctx.arc(item.fx, item.y + 56, 15, 0, getRadian(360), false);
+    // ctx.fill();
+    item.fx += 9;
+  })
+}
+
+
+let spaceCount = 0;
 
 const frameLoop = () => {  //requestAnimationFrame 재귀함수
   setUser(); //용용이 초기세팅.
+  setAttack();
   if(user.y >= 605 && spaceCount == 2){
     spaceCount = 0;
   }
-  console.log(user.y)
   requestAnimationFrame(frameLoop); // 프레임 재귀
 };
 
 
+
+
+//스페이스키
     document.addEventListener("keydown", (e) => {
      if (e.code === "Space") {
        e.preventDefault();
@@ -62,6 +122,14 @@ const frameLoop = () => {  //requestAnimationFrame 재귀함수
      }
    });
 
+//컨트롤키
+  
+
+  //  document.addEventListener('keyup', (e)=>{
+  //   if(!e.ctrlKey) {
+  //     fire.isAttack = false;
+  //   }
+  //  })
 
 
 
@@ -82,5 +150,5 @@ const frameLoop = () => {  //requestAnimationFrame 재귀함수
 
 
 canvas.addEventListener("click", (e) => {
-  console.log(e);
+  console.log(e.offsetX, e.offsetY);
 });
