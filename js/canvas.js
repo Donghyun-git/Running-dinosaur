20a3401aba449b10d0516ccd33785936547858bc
donghyun-git <donghyun1113@hanmail.net>
2023-04-14 19:02:36 +0900

    delete code comment

--- js/canvas.js
+++ js/canvas.js
@@ -1,19 +1,19 @@
 const canvas = document.querySelector('.canvas');
 const ctx = canvas.getContext('2d');
 
-class User {  //유저 클래스 정의
+class User {  
     constructor(x, y, isJump, isAttack, keyStack){
-        this.x = x;  //캔버스 x좌표 값
-        this.y = y;  //캔버스
-        this.isJump = isJump; //스페이스바 키 다운 여부
-        this.isAttack = isAttack; // 컨트롤키 다운 여부.
-        this.keyStack = keyStack; //스페이스바 키 몇번눌렀는지 카운트
+        this.x = x;  
+        this.y = y; 
+        this.isJump = isJump; 
+        this.isAttack = isAttack; 
+        this.keyStack = keyStack; 
     }
-}    //이게 캔버스 width랑 height를 정의해놓고 css로 건드려놔서 실제 좌표랑 달러. 그래서 아까 클릭이벤트로 좌표값 찍어본거야 ..ㅠㅠ
+}   
 
 class Fire extends User {
   constructor(x, y, isJump, isAttack, keyStack, fx, isConflict) {
-    super(x,y, isAttack);
+    super(x, y, isAttack);
     this.isJump = isJump;
     this.keyStack = keyStack;
     this.fx = fx;
@@ -21,7 +21,7 @@ class Fire extends User {
   };
 };
 
-class SmallBlock {  //작은 장애물 
+class SmallBlock {  
   constructor(x, y){
     this.x = x;
     this.y = y;
@@ -38,7 +38,7 @@ class BigBlock {
 
 let frameCount = 1;
 
-const user = new User(80, 600, false, false, 2);  // 유저 인스턴스 생성, (초기 x값, 초기 y값, 점프키 누름 여부, 컨트롤키 누름여부, 스페이스바 키 스택 초기값 == 0)
+const user = new User(80, 600, false, false, 2);  
 const attackList = [];
 const smallBlocksList = [];
 const bigBlocksList = [];
@@ -50,20 +50,17 @@ const getRadian = (d) => {
 const user1 = new Image();
 user1.src = "../img/user01.png";
 user1.onload = () => {
-  // 이미지가 로드된 후 실행되는 함수
   setUser();
 };
 const user2 = new Image();
 user2.src = "../img/user02.png";
 user2.onload = () => {
-  // 이미지가 로드된 후 실행되는 함수
   setUser();
 };
 
 const user3 = new Image();
 user3.src = "../img/user02.png";
 user3.onload = () => {
-  // 이미지가 로드된 후 실행되는 함수
   setUser();
 };
 
@@ -79,23 +76,20 @@ const setUser = () => {
       ctx.drawImage(user3, user.x, user.y, 150, 200);
     }
     if (user.isJump === true) {
-      // 스페이스바키 눌러서 isjump true일 시 점프함. 매 프레임마다 8 픽셀씩 위로 올라감. 한마디로 점프속도
       user.y -= 8;
       if (user.y <= 300) {
-        // 만약 용용이 y좌표가 300 이하면 isjump false로 바꿔서 더이상 안올라가게 하는거. 
         user.isJump = false;
       }
     }
 
-  if(user.y < 600 && user.isJump === false){ // 용용이가 땅바닥 위에 있고 isJump 가 false면은 용용이 내려감. 근데 이제 700 이하로 계속 떨어지기 때문에 첫번째 조건에서 걸려서 땅바닥에 착지하면 더이상 안내려가는거양 
-    user.y += 8; //여기서 +8을 계속 해주고 있자나
+  if(user.y < 600 && user.isJump === false){ 
+    user.y += 8; 
   }
 }
 
 const fireBall = new Image();
 fireBall.src = '../img/fireball.png';
 fireBall.onload = () => {
-  // 이미지가 로드된 후 실행되는 함수
   setAttack();
 };
 
@@ -107,18 +101,29 @@ const setAttack = () => {
   })
 }
 
+
+const mushRoom = new Image();
+mushRoom.src = "../img/mushroom.png";
+mushRoom.onload = () => {
+  setBlocks();
+};
+
 const setBlocks = () => {
   smallBlocksList.forEach(block => {
-    ctx.fillStyle = "red";
-    ctx.fillRect(block.x, block.y, 80, 130);
+    ctx.drawImage(mushRoom, block.x, block.y - 30, 80, 130);
     block.x -= 4;
   });
 }
 
+const bigmushRoom = new Image();
+bigmushRoom.src = "../img/bigmushroom.png";
+bigmushRoom.onload = () => {
+  setBigBlocks();
+};
+
 const setBigBlocks = () => {
   bigBlocksList.forEach(block => {
-    ctx.fillStyle = "blue";
-    ctx.fillRect(block.x, block.y, 300, 450);
+    ctx.drawImage(bigmushRoom, block.x, block.y - 30, 300, 450);
     block.x -= 0.3;
     console.log(block.x);
   })
@@ -126,11 +131,11 @@ const setBigBlocks = () => {
 
 let spaceCount = 0;
 
-const frameLoop = () => {  //requestAnimationFrame 재귀함수
+const frameLoop = () => {  
   ctx.clearRect(0, 0, 3000, 3000); 
   setBigBlocks();
   setBlocks();
-  setUser(); //용용이 초기세팅.
+  setUser(); 
   setAttack();
 
   if(user.y >= 600 && spaceCount == 2){
@@ -148,7 +153,7 @@ const frameLoop = () => {  //requestAnimationFrame 재귀함수
 
 
   frameCount++;
-  requestAnimationFrame(frameLoop); // 프레임 재귀
+  requestAnimationFrame(frameLoop); 
 };
 
 
@@ -159,8 +164,8 @@ document.addEventListener("keydown", (e) => {
   if (e.code === "Space") {
     e.preventDefault();
     if (!user.isJump && spaceCount < user.keyStack) {
-      user.isJump = true; // 스페이스바 키 누르면 용용이 isJump true
-      spaceCount++; // 스페이스바 키 누를때마다 용용이 키스택 증가
+      user.isJump = true; 
+      spaceCount++; 
       console.log(spaceCount);
     }
   }
